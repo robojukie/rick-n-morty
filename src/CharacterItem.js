2c29797d766ce731e53e1ceddc885024da61e57d
Julie Paik <juliepaik@JP-MBP.local>
2021-04-20 11:25:24 -0500

    add css

--- src/CharacterItem.js
+++ src/CharacterItem.js
@@ -4,13 +4,14 @@ function CharacterItem(props) {
   const { id, name, status, gender, species, image } = props;
   
   return (
-    <Link
-      to={`/character/${id}`}
-    >
-      <img src={image} alt='character profile' />
-      <div>{name}</div>
-      <div>{status}</div>
-    </Link>
+    <li className='character-item'>
+      <Link
+        to={`/character/${id}`}
+      >
+        <div className='character-thumbnail' style={{backgroundImage: `url(${image})`}}></div>
+        <div className='character-summary'>{name}</div>
+      </Link>
+    </li>
   )
 }
 
